e4ca7a1f0ee687647e0a6030ecda769fd6345fd3
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-23 16:18:19 -0400

    task/S-3: Created the BaseValidatable for the Models and added the new folder to the csproj

---
+++ Truextend/Scheduling/Logic/Models/Validation/IValidatable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Truextend.Scheduling.Logic.Models.Validation
+{
+	public interface IValidatable
+	{
+        bool IsValid();
+
+        IEnumerable<ValidationError> GetErrors();
+    }
+}
+
