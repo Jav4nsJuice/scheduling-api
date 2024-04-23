d092db2053d25e585dd4117b0abefc67e53551c2
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-23 15:11:53 -0400

    task/S-2: Added the Logger Information and AddCors

--- Truextend/Scheduling/Presentation/Program.cs
+++ Truextend/Scheduling/Presentation/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
 
 namespace Truextend.Scheduling.Presentation
 {
@@ -13,6 +15,24 @@ namespace Truextend.Scheduling.Presentation
             builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
             builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
             builder.Configuration.AddEnvironmentVariables();
+
+            IConfigurationSection LoggerPath = builder.Configuration.GetSection("Logger").GetSection("LogPath");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel
+                .Information()
+                .WriteTo.File(LoggerPath.Value, LogEventLevel.Information)
+                .CreateLogger();
+            Log.Information($"Json file setup has been read: appsettings.{builder.Environment.EnvironmentName}.json");
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin",
+                    builder => builder
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin()
+                );
+            });
 
             // Add services to the container.
 
