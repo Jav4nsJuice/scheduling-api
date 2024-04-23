37abbed96550477ab8caa3c57b9ca71aacb5408e
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-23 17:43:53 -0400

    feature/S-5: Added the Student Entity

---
+++ Truextend/Scheduling/Data/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Truextend.Scheduling.Data.Models.Base;
+
+namespace Truextend.Scheduling.Data.Models
+{
+	public class Student : Entity
+	{
+		[Required]
+		public string FirstName { get; set; }
+
+		[Required]
+		public string LastName { get; set; }
+	}
+}
+
