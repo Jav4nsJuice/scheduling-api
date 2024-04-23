53fc0ac26b173d3f6e7579c7dc84553f2447464b
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-23 15:49:53 -0400

    task/S-3: Created the base for the Base Controller and the IGenericManager

---
+++ Truextend/Scheduling/Logic/Managers/Base/IGenericManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Truextend.Scheduling.Logic.Managers.Base
+{
+	public interface IGenericManager<T> where T : class
+	{
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetById(Guid id);
+
+        Task<T> Create(T item);
+
+        Task<T> Update(T item, Guid id);
+
+        Task<bool> Delete(Guid itemId);
+    }
+}
+
---
+++ Truextend/Scheduling/Presentation/Controllers/Base/BaseSchedulingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Truextend.Scheduling.Logic.Managers.Base;
+
+namespace Truextend.Rewards.Presentation.Controllers.Base
+{
+	[ApiController]
+	public class BaseSchedulingController<T> : ControllerBase where T : class
+	{
+        private readonly IGenericManager<T> _classManager;
+
+        public BaseSchedulingController(IGenericManager<T> ClassManager)
+		{
+			_classManager = ClassManager;
+		}
+	}
+}
+
