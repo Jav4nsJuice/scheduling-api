438bee95d35b91cb57415f0a9d5263d1eb7d4e09
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-23 17:33:01 -0400

    feature/S-5: Added the StudentDto with a Validatable that allows only alphabets and spaces

---
+++ Truextend/Scheduling/Logic/Models/StudentDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Truextend.Scheduling.Logic.Models.Validation;
+
+namespace Truextend.Scheduling.Logic.Models
+{
+	public class StudentDto : BaseValidatable, IValidatable
+	{
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override bool IsValid()
+        {
+            Regex regex = new Regex(@"^[a-zA-Z\s]+$");
+            if (!regex.IsMatch(FirstName))
+            {
+                AddError(new ValidationError
+                {
+                    Field = "FirstName",
+                    Message = "FirstName can't have numbers or special characters."
+                });
+            }
+            return !Errors.Any();
+        }
+    }
+}
+
