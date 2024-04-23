b6080c8b12205548aa472308c18bfc80c2e55c7f
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-23 16:11:47 -0400

    task/S-3: Added the AlreadyExist Exception

---
+++ Truextend/Scheduling/Logic/Exceptions/AlreadyExistException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Truextend.Scheduling.Logic.Exceptions
+{
+	public class AlreadyExistException : Exception
+	{
+		public AlreadyExistException(string message) : base(message) { }
+
+        public AlreadyExistException(string message, Exception innerException) : base(string.Format(message), innerException) { }
+    }
+}
+
