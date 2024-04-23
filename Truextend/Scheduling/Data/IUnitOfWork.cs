c6b03093150660c077f2fe8b80b17ab86cc0c2c1
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-23 17:51:15 -0400

    feature/S-5: Added the uow interface with the StudentRepository interface in it

---
+++ Truextend/Scheduling/Data/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Truextend.Scheduling.Data.Repository.Interfaces;
+
+namespace Truextend.Scheduling.Data
+{
+	public interface IUnitOfWork
+	{
+		IStudentRepository StudentRepository { get; }
+
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollBackTransaction();
+        void Save();
+    }
+}
+
