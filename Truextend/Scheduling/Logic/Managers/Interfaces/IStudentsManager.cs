cf21866e8b7e58486bc42218c524ae4e9c49e0d0
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-24 18:22:05 -0400

    feature/S-8: Implemented the logic that the endpoint is going to use

--- Truextend/Scheduling/Logic/Managers/Interfaces/IStudentsManager.cs
+++ Truextend/Scheduling/Logic/Managers/Interfaces/IStudentsManager.cs
@@ -1,8 +1,13 @@
-﻿using Truextend.Scheduling.Logic.Managers.Base;
+﻿using System;
+using System.Threading.Tasks;
+using Truextend.Scheduling.Logic.Managers.Base;
 using Truextend.Scheduling.Logic.Models;
 
 namespace Truextend.Scheduling.Logic.Managers.Interfaces
 {
-	public interface IStudentsManager : IGenericManager<StudentDto> { }
+	public interface IStudentsManager : IGenericManager<StudentDto>
+	{
+        Task<StudentCourseDto> AddToCourse(Guid studentId, Guid courseId);
+    }
 }
 
