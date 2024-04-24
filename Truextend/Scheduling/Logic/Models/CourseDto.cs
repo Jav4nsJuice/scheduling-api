56ae0fb0c1a4944055306d69c966b8da753c37fa
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-24 16:00:13 -0400

    feature/S-6: Created the CourseDto

---
+++ Truextend/Scheduling/Logic/Models/CourseDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Truextend.Scheduling.Logic.Models.Validation;
+
+namespace Truextend.Scheduling.Logic.Models
+{
+	public class CourseDto : BaseValidatable, IValidatable
+	{
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public override bool IsValid()
+        {
+            if (string.IsNullOrWhiteSpace(Title))
+            {
+                AddError(new ValidationError
+                {
+                    Field = "Title",
+                    Message = "Title can't be created with white spaces."
+                });
+            }
+            return !Errors.Any();
+        }
+    }
+}
+
