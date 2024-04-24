c9bb9ad02d37428b4f5f56768855d3a54d57a248
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-24 16:02:27 -0400

    feature/S-6: Created the Course entity

---
+++ Truextend/Scheduling/Data/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Truextend.Scheduling.Data.Models.Base;
+
+namespace Truextend.Scheduling.Data.Models
+{
+	public class Course : Entity
+	{
+        [Required]
+        public string Title{ get; set; }
+
+        public string Description { get; set; }
+    }
+}
+
