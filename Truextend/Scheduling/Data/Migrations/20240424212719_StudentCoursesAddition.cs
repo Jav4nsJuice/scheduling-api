46b87a043ce3a74d8167895ac0ef4a23a4c16938
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-24 17:29:30 -0400

    feature/S-7: Created new migration for the StudentCourses new table

---
+++ Truextend/Scheduling/Data/Migrations/20240424212719_StudentCoursesAddition.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Truextend.Scheduling.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentCoursesAddition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentCourse",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StudentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CourseId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentCourse_Id", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudentCourse_CourseId",
+                        column: x => x.CourseId,
+                        principalSchema: "dbo",
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentCourse_StudentId",
+                        column: x => x.StudentId,
+                        principalSchema: "dbo",
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentCourse_CourseId",
+                schema: "dbo",
+                table: "StudentCourse",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentCourse_StudentId",
+                schema: "dbo",
+                table: "StudentCourse",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentCourse",
+                schema: "dbo");
+        }
+    }
+}
