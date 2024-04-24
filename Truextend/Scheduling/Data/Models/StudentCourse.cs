e70ab1ff342baaaf457e60a9e59ca969726edb55
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-24 16:59:03 -0400

    feature/S-7: Created the StudentCourse Dto and the entity

---
+++ Truextend/Scheduling/Data/Models/StudentCourse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Truextend.Scheduling.Data.Models.Base;
+
+namespace Truextend.Scheduling.Data.Models
+{
+    [Table("StudentCourse")]
+    public class StudentCourse : Entity
+	{
+        [Required]
+        public Guid StudentId { get; set; }
+
+        [Required]
+        public Guid CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
+
