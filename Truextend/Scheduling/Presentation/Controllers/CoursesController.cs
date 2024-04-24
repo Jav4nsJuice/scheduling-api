788e4ce7a4b41699eb084bc0f4d3df678d9a71ee
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-24 16:15:36 -0400

    feature/S-6: Updated the Transient in Program.cs and created the controller file

---
+++ Truextend/Scheduling/Presentation/Controllers/CoursesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Truextend.Scheduling.Logic.Managers.Interfaces;
+using Truextend.Scheduling.Logic.Models;
+using Truextend.Scheduling.Presentation.Controllers.Base;
+
+namespace Truextend.Scheduling.Presentation.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/courses")]
+    public class CoursesController : BaseSchedulingController<CourseDto>
+    {
+        public CoursesController(ICoursesManager coursesManager) : base(coursesManager) { }
+    }
+}
+
