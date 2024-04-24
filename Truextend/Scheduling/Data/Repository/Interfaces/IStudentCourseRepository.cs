42c72bccc738f0a81465cad02909ba35baa26368
Javier Ferrel <javans.ferrel@gmail.com>
2024-04-24 17:24:02 -0400

    feature/S-7: Added the studentCourse repository and interface

---
+++ Truextend/Scheduling/Data/Repository/Interfaces/IStudentCourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Truextend.Scheduling.Data.Models;
+using Truextend.Scheduling.Data.Repository.Base;
+
+namespace Truextend.Scheduling.Data.Repository.Interfaces
+{
+	public interface IStudentCourseRepository : IRepository<StudentCourse>
+	{
+        Task<IEnumerable<StudentCourse>> GetAllByStudentIdAsync(Guid studentId);
+
+        Task<IEnumerable<StudentCourse>> GetAllByStudentIdAndCourseIdAsync(Guid studentId, Guid courseId);
+    }
+}
+
---
+++ Truextend/Scheduling/Data/Repository/StudentCourseRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Truextend.Scheduling.Data.Models;
+using Truextend.Scheduling.Data.Repository.Base;
+using Truextend.Scheduling.Data.Repository.Interfaces;
+
+namespace Truextend.Scheduling.Data.Repository
+{
+	public class StudentCourseRepository : Repository<StudentCourse>, IStudentCourseRepository
+	{
+        protected readonly SchedulingDBContext _schedulingDbContext;
+        public StudentCourseRepository(SchedulingDBContext schedulingDbContext) : base(schedulingDbContext)
+        {
+            _schedulingDbContext = schedulingDbContext;
+        }
+
+        public async Task<IEnumerable<StudentCourse>> GetAllByStudentIdAndCourseIdAsync(Guid studentId, Guid courseId)
+        {
+            return await _schedulingDbContext
+               .Set<StudentCourse>()
+               .Where(u => u.StudentId == studentId && u.CourseId == courseId)
+               .ToListAsync();
+        }
+
+        public async Task<IEnumerable<StudentCourse>> GetAllByStudentIdAsync(Guid studentId)
+        {
+            return await _schedulingDbContext
+                .Set<StudentCourse>()
+                .Where(u => u.StudentId == studentId)
+                .ToListAsync();
+        }
+    }
+}
+
