d2c7085a6d4e499037693e547c8a4f4bffda9b0d
Javier Ferrel <javans.ferrel@gmail.com>
2024-05-06 10:20:38 -0400

    task/S-11: Added data seeding base

---
+++ Truextend/Scheduling/Data/Data Seeding/Course_202405061002.sql
@@ -0,0 +1,11 @@
+INSERT INTO LocalSchedulingDB.dbo.Course (Id,Title,Description) VALUES
+	 (N'40578677-0D77-4C22-CEAD-08DC68E2DC40',N'[MAT-01] Mathematicians',N'This is a Mathematicians 01 class.'),
+	 (N'60DDE7B4-CA0F-469C-CEAE-08DC68E2DC40',N'[PHY-01] Physics Fundamentals',N'Introduction to basic principles of physics.'),
+	 (N'8C92421D-C9B2-4086-CEAF-08DC68E2DC40',N'[CHEM-01] Chemistry Basics',N'Exploring the fundamentals of chemistry.'),
+	 (N'320232C2-867D-4192-CEB0-08DC68E2DC40',N'[BIO-01] Introduction to Biology',N'Overview of biological concepts and principles.'),
+	 (N'8FB262F5-764B-4990-CEB1-08DC68E2DC40',N'[ENG-01] English Grammar',N'Study of grammar rules in the English language.'),
+	 (N'5D36E80F-1B26-462C-CEB2-08DC68E2DC40',N'[HIS-01] World History',N'Examining key events and developments in world history.'),
+	 (N'2BC59084-12E2-4D17-CEB3-08DC68E2DC40',N'[GEO-01] Introduction to Geography',N'Understanding basic concepts in geography.'),
+	 (N'77506521-0944-4CE6-CEB4-08DC68E2DC40',N'[ART-01] Introduction to Art History',N'Exploring major movements and styles in art history.'),
+	 (N'A69F34F3-9321-4CE0-CEB5-08DC68E2DC40',N'[CS-01] Computer Science Fundamentals',N'Introduction to basic concepts in computer science.'),
+	 (N'EB94E47A-A316-475F-CEB6-08DC68E2DC40',N'[MUS-01] Music Appreciation',N'Learning about various genres and styles of music.');
---
+++ Truextend/Scheduling/Data/Data Seeding/Student_202405061003.sql
@@ -0,0 +1,18 @@
+INSERT INTO LocalSchedulingDB.dbo.Student (Id,FirstName,LastName) VALUES
+	 (N'478639CD-29D8-48E7-99D3-08DC68DFADCC',N'Javier Alejandro',N'Ferrel Rivera'),
+	 (N'BC9DA6DE-5CED-4144-99D4-08DC68DFADCC',N'Gabriela Sofia',N'Montes de Oca'),
+	 (N'BFBBDD48-E0E5-4C65-99D5-08DC68DFADCC',N'Diego Alonso',N'Hernandez Salazar'),
+	 (N'0B004D19-84E3-413D-99D6-08DC68DFADCC',N'Valentina Isabel',N'Gomez Alvarado'),
+	 (N'8CEC8EB7-440A-4849-99D7-08DC68DFADCC',N'Matias Alejandro',N'Vargas Lopez'),
+	 (N'9A55C24F-0B4E-44EB-99D8-08DC68DFADCC',N'Luciana Victoria',N'Ramirez Soto'),
+	 (N'84753050-E905-49E7-99D9-08DC68DFADCC',N'Sebastian Andres',N'Castillo Mendez'),
+	 (N'C5E00B2A-4CF2-4B07-99DA-08DC68DFADCC',N'Camila Daniela',N'Silva Castro'),
+	 (N'C2B00A66-1E6A-4C23-99DB-08DC68DFADCC',N'Martin Ignacio',N'Diaz Garcia'),
+	 (N'8F3E7C56-5DD5-4937-99DC-08DC68DFADCC',N'Isabella Sofia',N'Perez Molina');
+INSERT INTO LocalSchedulingDB.dbo.Student (Id,FirstName,LastName) VALUES
+	 (N'B0FB7082-B2A9-463A-99DD-08DC68DFADCC',N'Juan Pablo',N'Lopez Martinez'),
+	 (N'C57E82D0-5066-4A01-99DE-08DC68DFADCC',N'Antonella Lucia',N'Gonzalez Diaz'),
+	 (N'47B9509B-2B33-4730-99DF-08DC68DFADCC',N'Nicolas Alejandro',N'Rios Fernandez'),
+	 (N'E4D6353B-F480-44AA-99E0-08DC68DFADCC',N'Mariana Alejandra',N'Torres Rodriguez'),
+	 (N'6CC9242F-8B84-400C-99E1-08DC68DFADCC',N'Emilio Jose',N'Sanchez Cruz'),
+	 (N'78AE3C67-EE5E-46F8-6D28-08DC6AC778AF',N'Jorge Antonio',N'Moreno Ponce');
